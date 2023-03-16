58e4817207444ea112e55af0a6a9c4ac7b3328f5
SowmyaGorripati <116424393+SowmyaGorripati@users.noreply.github.com>
2023-03-17 00:08:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+let span = document.createElement("div");
+span.className = "show";
+
+document.body.append(span);
+
+let count = 11;
+setTimeout(() => {
+    count = count - 1;
+    span.innerHTML = count ; 
+    setTimeout(() => {
+        count = count - 1;
+        span.innerHTML = count ; 
+        setTimeout(() => {
+            count = count - 1;
+            span.innerHTML = count ; 
+            setTimeout(() => {
+                count = count - 1;
+                span.innerHTML = count ; 
+                setTimeout(() => {
+                    count = count - 1;
+                    span.innerHTML = count ; 
+                    setTimeout(() => {
+                        count = count - 1;
+                        span.innerHTML = count ; 
+                        setTimeout(() => {
+                            count = count - 1;
+                            span.innerHTML = count ; 
+                            setTimeout(() => {
+                                count = count - 1;
+                                span.innerHTML = count ; 
+                                setTimeout(() => {
+                                    count = count - 1;
+                                    span.innerHTML = count ; 
+                                    setTimeout(() => {
+                                        count = count - 1;
+                                        span.innerHTML = count ;
+                                        setTimeout(() => {
+                                            count = count - 1;
+                                            span.innerHTML = count ;
+                                            setTimeout(() => {
+                                                count = count - 1;
+                                                span.innerHTML = count ; 
+                                                span.innerHTML = "HAPPY INDEPENDECE DAY";
+                                            },1000);
+                                        },1000);
+                                    },1000);    
+                                },1000);    
+                            },1000);    
+                        },1000);    
+                    },1000);    
+                },1000);    
+            },1000);    
+        },1000);    
+    },1000);
+},1000);
