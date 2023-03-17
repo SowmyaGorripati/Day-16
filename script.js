4057d5e094bf569ea81a65aaba53b389a2435fd6
SowmyaGorripati <116424393+SowmyaGorripati@users.noreply.github.com>
2023-03-17 09:10:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ setTimeout(() => {
                                             setTimeout(() => {
                                                 count = count - 1;
                                                 span.innerHTML = count ; 
-                                                span.innerHTML = "HAPPY BIRTHDAY DAY";
+                                                span.innerHTML = "HAPPY BIRTHDAY DAY CHECHYYY";
                                             },1000);
                                         },1000);
                                     },1000);    
